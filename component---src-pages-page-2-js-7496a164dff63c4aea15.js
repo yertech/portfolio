c65153988ef9065859fc17eed9936fd724543add
fabrice <ftrey@yertech.org>
2019-10-16 16:38:10 +0800

    Updates

--- component---src-pages-page-2-js-7496a164dff63c4aea15.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{477:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(64),c=a(473),r=a(472);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-7496a164dff63c4aea15.js.map
