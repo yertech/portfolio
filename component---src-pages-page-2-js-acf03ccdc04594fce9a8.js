f3993fd506d9b3697b19c4d3fcaf21ccb5c8ac0c
fabrice <ftrey@yertech.org>
2019-10-16 13:02:33 +0800

    Updates

--- component---src-pages-page-2-js-acf03ccdc04594fce9a8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{478:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(64),c=a(474),r=a(473);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-acf03ccdc04594fce9a8.js.map
