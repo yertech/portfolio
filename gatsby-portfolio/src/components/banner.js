38f38c7f692ad2ba570755609d6fd8672014824e
fabrice <ftrey@yertech.org>
2019-10-22 19:07:55 +0800

    feat: add react-animations and styled-components

--- gatsby-portfolio/src/components/banner.js
+++ gatsby-portfolio/src/components/banner.js
@@ -1,21 +1,43 @@
 import React from "react"
 import PropTypes from "prop-types"
 import ReactHtmlParser from 'react-html-parser'; 
+import styled, { keyframes } from 'styled-components';
+import { fadeInUp, fadeInDown } from 'react-animations';
+
+
 
 const Banner = ({ btnData, bannerPageData }) => {
+	
+	const fadeInUpAnimation = keyframes`${fadeInUp}`;
+	const fadeInDownAnimation = keyframes`${fadeInDown}`;
+	const FadeInUpDiv = styled.div`
+	animation: 2s 2.2s ${fadeInUpAnimation};
+	`;
+	const FadeInUpDivLong = styled.div`
+	animation: 2s 2.7s ${fadeInUpAnimation};
+	`;
+	const FadeInDownDiv = styled.div`
+	animation: 2s 2.2s ${fadeInDownAnimation};
+	`;
+
+
     // <!-- start banner Area -->
 	return (
 	<section className="home-banner-area">
 		<div className="container">
 			<div className="row fullscreen d-flex align-items-center">
 				<div className="banner-content col-lg-6 col-md-12 justify-content-center ">
-					<div className="me wow fadeInDown" data-wow-duration="1s" data-wow-delay="1.2s">{bannerPageData.banner_msg}</div>
-					<h1 className="wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.7s">{bannerPageData.banner_name}</h1>
-					<div className="designation mb-50 wow fadeInUp" data-wow-duration="1s" data-wow-delay="2.1s">
-					{ReactHtmlParser(bannerPageData.banner_desc)}
-					</div>
+					<FadeInDownDiv className="me">
+						{bannerPageData.banner_msg}
+					</FadeInDownDiv>
+					<FadeInUpDiv>
+						<h1>{bannerPageData.banner_name}</h1>
+					</FadeInUpDiv>
+					<FadeInUpDivLong className="designation mb-50">							
+						{ReactHtmlParser(bannerPageData.banner_desc)}
+					</FadeInUpDivLong>
 					<a href="mailto:ftrey@yertech.org" className="primary-btn" data-text={btnData.hireme_title}>
-					{ReactHtmlParser(btnData.hireme)}
+						{ReactHtmlParser(btnData.hireme)}
 					</a>
 				</div>
 			</div>
